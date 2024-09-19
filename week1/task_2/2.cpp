d50f02d86e692470336cdc3453d910ce68942ff9
Karina Basova <areyouthere71@gmail.com>
2024-09-19 16:42:23 +0300

    Задача 2

---
+++ week1/task_2/2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const double g = 9.8;    // Задаём константу - ускорение свободного падения
+
+int main() {
+    setlocale(0, "Rus");  // Русский язык в консоли
+
+    cout << "Задача № 2. Бросок камня\n\n";
+
+    double T, x, H, V, alpha, y;
+
+    // Вводим входные данные
+    cout << "Введите значение H (высота обрыва): ";
+    cin >> H;
+    cout << "Введите значение V (начальная скорость камня): ";
+    cin >> V;
+    cout << "Введите значение alpha: ";
+    cin >> alpha;
+
+    // Вычисляем время полёта и положение камня (координаты) по формулам
+    T = (V * sin(alpha) / g) * (1 + sqrt(1 + (2 * g * H) / (pow(V, 2) * pow(sin(alpha), 2))));
+    x = V * T * cos(alpha);
+    y = H + V * T * sin(alpha) - 1 / 2 * g * pow(T, 2);
+
+    cout << "x(T) = " << x << "\ny(T) = " << y << endl;
+
+    return 0;
+}
